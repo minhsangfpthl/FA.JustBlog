3e691cdba152eb4421a1b5b1906adefed5129363
sangbmhe140526@fpt.edu.vn <sangbmhe140526@fpt.edu.vn>
2021-07-31 22:28:18 +0700

    Create post category and tag models

---
+++ src/FA.JustBlog/FA.JustBlog.Models/BaseEntities/IBaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FA.JustBlog.Models.BaseEntities
+{
+    public interface IBaseEntity
+    {
+        Guid Id { get; set; }
+
+        bool IsDeleted { get; set; }
+
+        DateTime InsertedAt { get; set; }
+
+        DateTime UpdatedAt { get; set; }
+
+    }
+}
