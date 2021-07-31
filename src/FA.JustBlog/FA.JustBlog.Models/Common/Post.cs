ea93c371c475727d02f7b63dd1ff210536b39fc1
sangbmhe140526@fpt.edu.vn <sangbmhe140526@fpt.edu.vn>
2021-07-31 23:54:17 +0700

    Create common project and complete repositories and services with unitofwork

--- src/FA.JustBlog/FA.JustBlog.Models/Common/Post.cs
+++ src/FA.JustBlog/FA.JustBlog.Models/Common/Post.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FA.JustBlog.Models.BaseEntities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
