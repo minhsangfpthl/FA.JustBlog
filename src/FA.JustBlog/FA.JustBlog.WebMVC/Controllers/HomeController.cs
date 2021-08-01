d9e62906de111883489f0f47ec35fea6cb8199e8
sangbmhe140526@fpt.edu.vn <sangbmhe140526@fpt.edu.vn>
2021-08-01 22:12:16 +0700

    Fix

--- src/FA.JustBlog/FA.JustBlog.WebMVC/Controllers/HomeController.cs
+++ src/FA.JustBlog/FA.JustBlog.WebMVC/Controllers/HomeController.cs
@@ -1,9 +1,5 @@
 ﻿using FA.JustBlog.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using System.Web;
 using System.Web.Mvc;
 
 namespace FA.JustBlog.WebMVC.Controllers
